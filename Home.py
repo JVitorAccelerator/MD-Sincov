2c7c5f6a19ecb19fcf76f0c9372ad3e71c211fcd
netoids <netoid07@gmail.com>
2023-08-10 19:22:56 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,22 +1,44 @@
 import streamlit as st
 
-title = "Perguntas relacionado ao SINCOV e análise da execução dos convênios"
 
 def main():
     with st.container():
-        st.title(title)
+        st.title("Perguntas relacionado ao SINCOV e análise da execução dos convênios")
+        
+        st.write("")
+    
+    
 
     with st.container():
 
+        
         st.header('Perguntas')
 
-        objectives =('<ul>' \
-            '<li class="content-size">As propostas gastaram mais ou menos na execução dos convênios?</li>'\
-            '<li class="content-size">Existe alguma relação entre o tamanho do repasse das propostas e o valor gasto na execução dos convênios?</li>'\
-            '<li class="content-size"> Quais são os principais tipos de projetos que receberam convênios e como eles se comparam em termos de repasses e gastos?</li>'\
-            '<li class="content-size"> "Qual é a tendência ao longo do tempo em relação ao valor médio de repasse e gastos em convênios?</li>'\
+        st.write("")
+
+
+        objectives =(
+
+
+            '<ul>' \
+            '<li class="content-size">As propostas gastaram mais ou menos na execução dos convênios?</li><br>'\
+            '<li class="content-size">Existe alguma relação entre o tamanho do repasse das propostas e o valor gasto na execução dos convênios?</li><br>'\
+            '<li class="content-size"> Quais são os principais tipos de projetos que receberam convênios e como eles se comparam em termos de repasses e gastos?</li><br>'\
+            '<li class="content-size"> "Qual é a tendência ao longo do tempo em relação ao valor médio de repasse e gastos em convênios?</li><br>'\
             '</ul>'
+
+
+
+            """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """    
         )
+
+
         st.markdown(objectives, unsafe_allow_html=True)
 
 
