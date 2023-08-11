3af5c16e300d13362cc52b64317243d06bb24efa
artillis.prado <aadejarde15@gmail.com>
2023-08-10 21:39:37 -0300

    adicionando exemplos de analises

--- pages/Plots.py
+++ pages/Plots.py
@@ -1,5 +1,7 @@
 from Classes import dataframe
 import streamlit as st
+import pandas as pd
+import plotly_express as px
 
 df_convenio = dataframe.Dados.dimconvenio
 df_data = dataframe.Dados.dimdata
@@ -9,5 +11,21 @@ df_parlamentar = dataframe.Dados.dimparlamentar
 df_propostas = dataframe.Dados.dimproposta
 df_fato = dataframe.Dados.fatoexecucao
 
-st.table(df_convenio)
 
+#  ----- Gráfico de Total insvestido por ano ------
+df_dataYear = df_data[["keyData","data_id","mes_texto","ano_texto"]].copy()
+df_dataYear = df_dataYear.rename(columns={'keyData': 'datakey'})
+df_fato = df_fato.astype({'valorGlobal':'int'})
+result = pd.merge(df_dataYear, df_fato, how="inner", on=['datakey'])
+df1 = pd.DataFrame(result.groupby(by=['ano_texto'])['valorGlobal'].sum())
+df1.reset_index(inplace=True)
+st.table(df1)
+fig = px.bar(df1, 
+             x='ano_texto', 
+             y='valorGlobal', 
+             labels={
+                     'ano_texto': 'Ano',
+                     'valorGlobal': 'Valor total'
+                },
+             title="Total de investido por ano")
+st.write(fig)
