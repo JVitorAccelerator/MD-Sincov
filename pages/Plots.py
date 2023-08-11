f2a488d1f65d8c5942e4b9f0dde10cab84057e8c
artillis.prado <aadejarde15@gmail.com>
2023-08-11 09:43:47 -0300

    Documentando cada linha, para melhor entendimento

--- pages/Plots.py
+++ pages/Plots.py
@@ -13,12 +13,13 @@ df_fato = dataframe.Dados.fatoexecucao
 
 
 #  ----- Gráfico de Total insvestido por ano ------
-df_dataYear = df_data[["keyData","data_id","mes_texto","ano_texto"]].copy()
-df_dataYear = df_dataYear.rename(columns={'keyData': 'datakey'})
-df_fato = df_fato.astype({'valorGlobal':'int'})
-result = pd.merge(df_dataYear, df_fato, how="inner", on=['datakey'])
-df1 = pd.DataFrame(result.groupby(by=['ano_texto'])['valorGlobal'].sum())
-df1.reset_index(inplace=True)
+df_dataYear = df_data[["keyData","data_id","mes_texto","ano_texto"]].copy() # copiando algumas colunas do dataframe para um novo
+df_dataYear = df_dataYear.rename(columns={'keyData': 'datakey'}) # Renomeando nome de coluna para conseguir fazer o merge
+df_fato = df_fato.astype({'valorGlobal':'int'}) # Trocando para tipo int
+st.write(df_fato.shape) # Tamanho de linhas e colunas
+result = pd.merge(df_dataYear, df_fato, how="inner", on=['datakey']) # Juntando tabelas com base na chave datakey
+df1 = pd.DataFrame(result.groupby(by=['ano_texto'])['valorGlobal'].sum()) # Agrupando informações da tabela de ano e com a soma do valor global
+df1.reset_index(inplace=True) # Removendo index para a coluna ano aparecer
 st.table(df1)
 fig = px.bar(df1, 
              x='ano_texto', 
